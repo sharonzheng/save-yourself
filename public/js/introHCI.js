b264682e7666cc153a4ac9f368ca51943298079a
Sharon Zheng <sharonjzheng@gmail.com>
2016-10-26 13:21:55 -0700

    finished up hp config screen

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -11,3 +11,19 @@ $(document).ready(function() {
 function initializePage() {
 	// add any functionality and listeners you want here
 }
+
+$(".cross").hide();
+$(".menu").hide();
+$(".hamburger").click(function() {
+	$(".menu").slideToggle("slow", function() {
+		$(".hamburger").hide();
+		$(".cross").show();
+	});
+});
+
+$(".cross").click(function() {
+	$(".menu").slideToggle("slow", function() {
+		$(".cross").hide();
+		$(".hamburger").show();
+	});
+});
--- routes/hpconfig.js
+++ routes/hpconfig.js
@@ -2,3 +2,4 @@ exports.view =	function(req,	res)	{
 	var	name	=	req.params.name;
 		res.render('hpconfig');
 }
+
